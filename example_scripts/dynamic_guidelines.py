063a0617d6ea959f87e815b973e31819b4e6090a
Jono Everts <j.everts@adinstruments.com>
2025-02-12 11:23:43 +1300

    Added more comments

--- example_scripts/dynamic_guidelines.py
+++ example_scripts/dynamic_guidelines.py
@@ -1,4 +1,3 @@
-
 import win32com.client
 import time
 
@@ -16,7 +15,7 @@ def invoke_com_method(com_object, method_name, *args):
         print(f"Error invoking method '{method_name}': {e}")
 
 
-def getDataFromEndOfLastBlock(doc, channelNumber, blockNumber, duration):
+def getDataFromEndOfLastBlock(doc, channelNumber, duration):
     lastBlock = doc.NumberOfRecords -1
     secsPerTick = doc.GetRecordSecsPerTick(lastBlock)
     ticksPerSecond = 1/secsPerTick
@@ -28,44 +27,41 @@ def getDataFromEndOfLastBlock(doc, channelNumber, blockNumber, duration):
 
 # ------------------------------------------------------------------------------
 
-# Connect to 
+# Connect to Labchart
 labchart = win32com.client.Dispatch("ADIChart.Application") 
-filepath = "C:/Users/jeverts/Documents/Code/python_labchart_macros/test_file.adicht"                   # Enter the path to your labchart file e.g "C:/Users/yourname/Documents/your_file.adicht"
+filepath = "C:/Users/jeverts/Documents/Code/python_labchart_macros/test_file.adicht" # Enter the path to your labchart file e.g "C:/Users/yourname/Documents/your_file.adicht"
 doc = labchart.Open(filepath)   # Open the specified document in Labchart   
 
 
 # User Inputs ------------------------------------------------------------------
-channelForGuideLine = 0        # The channel you want to add guidelines to
+channelForGuideLine = 0         # The channel you want to add guidelines to
 percentageOfMax = 20	        # The percentage of the maximum you want the second guideline to be displayed at
-maximumColumnInDataPad = 1      # The column used on the datapad for keeping track of the maximum
 waitTimeBeforeRecalculate = 2   # The time to wait before recalculating the maximum
+
 # ------------------------------------------------------------------------------
 
-rollingMaximum = 0		        # Variable to hold the rolling maximum value
-
+# Variable to hold the rolling maximum value
+rollingMaximum = 0		        
 
 # Setup the two guidelines
 invoke_com_method(doc, "SetGuidelineValue", channelForGuideLine , 1, 0, "V", "")
 invoke_com_method(doc, "SetGuidelineValue", channelForGuideLine , 2, 0, "V", "")
 	
-# Begin SetGuidelinesInfo
+# Set guideline 1 to be visible
 Guideline = 1
 EnableGuideline = True
 ShowGuideline = True
 GuidelineColor = 9013641
-# doc.SetGuidelinesInfo(channelForGuideLine, Guideline, EnableGuideline, ShowGuideline, GuidelineColor)
 invoke_com_method(doc, "SetGuidelinesInfo", channelForGuideLine, Guideline, EnableGuideline, ShowGuideline, GuidelineColor)
-# End SetGuidelinesInfo
 
-# Begin SetGuidelinesInfo
+# Set guideline 2 to be visible
 Guideline = 2
 EnableGuideline = True
 ShowGuideline = True
 GuidelineColor = 9013641
 invoke_com_method(doc, "SetGuidelinesInfo", channelForGuideLine, Guideline, EnableGuideline, ShowGuideline, GuidelineColor)
-# End SetGuidelinesInfo
 	
-# Begin SetGuidelineRegionInfo
+# Set guideline shading to be green
 ShadeTop = False
 ShadeTopColor = 12763902
 ShadeMid = True
@@ -73,22 +69,16 @@ ShadeMidColor = 12975793
 ShadeBottom = False
 ShadeBottomColor = 12763902
 invoke_com_method(doc, "SetGuidelineRegionInfo", channelForGuideLine, ShadeTop, ShadeTopColor, ShadeMid, ShadeMidColor, ShadeBottom, ShadeBottomColor)
-# End SetGuidelineRegionInfo
+
+# Initialise the guidelines to 0
+invoke_com_method(doc, "SetGuidelineValue", channelForGuideLine, 1, 0, "V", "")
+invoke_com_method(doc, "SetGuideLineValue", channelForGuideLine, 2, 0 , "V", "")
 
 
-invoke_com_method(doc, "SetGuidelineValue", channelForGuideLine, 1, rollingMaximum, "V", "")
-invoke_com_method(doc, "SetGuideLineValue", channelForGuideLine, 2, rollingMaximum - percentageOfMax/100*rollingMaximum , "V", "")
-
-
-# Loop to calculate the maximum every 
+# Loop to check for a new maximum   
 while True:
     time.sleep(waitTimeBeforeRecalculate)
-    block_number = 1
-    start_sample = 1
-    n_samples = 10
-    chan_number = 1
-    start_time = 0
-    data = getDataFromEndOfLastBlock(doc, chan_number,block_number,waitTimeBeforeRecalculate)
+    data = getDataFromEndOfLastBlock(doc, channelForGuideLine + 1,waitTimeBeforeRecalculate)
     maxValueInMostRecentSelection = max(data)
     print("Rolling maximum:", rollingMaximum)
     print("Max in most recent data:", maxValueInMostRecentSelection, "\n")
